f7c1a9e7885ce2b5f6ac0cd25fa5bc2c38a01036
Caleb Eby <caleb.eby01@gmail.com>
2016-07-08 15:49:23 -0700

    add more functionality

--- scripts.js
+++ scripts.js
@@ -4,11 +4,39 @@ var $ = function(element) {
 
 var buttons = $('.buttons button[data-button]');
 var screen = $('.screen')[0];
+var enter = $('#enter')[0];
 
 buttons.forEach(function(button) {
   button.addEventListener('click', function() {
     var buttonValue = button.getAttribute('data-button');
-    console.log('button ' + buttonValue + ' clicked');
-    screen.innerHTML += buttonValue;
+    addToScreen(buttonValue);
   });
 });
+enter.addEventListener('click', function() {
+  var result = screen.innerHTML;
+  result = eval(result.replace('÷', '/').replace('×', '*'));
+  screen.innerHTML = result;
+  screen.classList.add('result')
+});
+var addToScreen = function(character) {
+  console.log('button ' + character + ' clicked');
+  if (screen.classList.contains('result')) {
+    screen.classList.remove('result');
+    screen.innerHTML = '';
+  }
+  screen.innerHTML += character;
+}
+document.addEventListener('keypress', function(event) {
+  var charCode = (event.which) ? event.which : event.keyCode;
+  if (isCalcKey(event)) {
+    addToScreen(String.fromCharCode(charCode));
+  }
+});
+var isCalcKey = function(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode == 46 || charCode > 31 && (charCode < 48 || charCode > 57)){
+        evt.preventDefault();
+        return false;
+    }
+    return true;
+}
